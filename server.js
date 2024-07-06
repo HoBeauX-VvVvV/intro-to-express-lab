68a189b747d8727a66a1e8533f4c306a44359fe5
BeauX <fire-fly_7@hotmail.com>
2024-07-06 13:52:06 -0700

    ex 2 commit

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const app = express();
 //app.use(morgan('dev'));
 
 app.get('/greet/:userName', (req, res) => {
-    console.log(req.params.userName)
     res.send(`<h1>Hello ${req.params.userName}, it's great to see you again</h1>`)
 })
 
-
+app.get('/roll/:number', (req, res) => {
+    if(isNaN(req.params.number)) {
+        res.send('<h1>You must enter a number</h1>')
+    } else {
+        res.send(`<h1>You rolled a ${req.params.number}</h1>`)
+    }
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
